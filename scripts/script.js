15d60599578da3ccf91b99748a13be88d1e8c013
Lewis <lewisbrignell@gmail.com>
2019-12-16 12:59:48 -0500

    nav done, other stuff too

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,14 @@
-console.log('success!');
+document.addEventListener("DOMContentLoaded", function () {
+    let menuButton = document.querySelector('#showMenu');
+    let closeButton = document.querySelector('#hideMenu');
+    let mainNav = document.querySelector('#mainNav');
+
+    menuButton.onclick = () => {
+        mainNav.style.left = 0;
+    }
+
+    closeButton.onclick = () => {
+        mainNav.style.left = '-1000px';
+    }
+});
+
